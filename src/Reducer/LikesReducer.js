22c906e398e559acdfda5a98ee6051efdf7e358e
Дмитрий Рыбин <rybin@tochkak.ru>
2016-10-03 22:52:30 +0300

    fixed syntax reducer

--- src/Reducer/LikesReducer.js
+++ src/Reducer/LikesReducer.js
@@ -11,7 +11,7 @@ const initialState = {
 const LikesReducer = handleActions({
     [actions.HANDLE_LIKE_CLICK]: state => ({
         ...state,
-        isLike: state.isLike ? false : true,
+        isLike: !state.isLike,
         isDislike: false,
         likesCount: state.isLike ? state.likesCount - 1 : state.likesCount + 1,
         dislikeCount: state.isDislike ? state.dislikeCount - 1 : state.dislikeCount,
@@ -19,7 +19,7 @@ const LikesReducer = handleActions({
     [actions.HANDLE_DISLIKE_CLICK]: state => ({
         ...state,
         isLike: false,
-        isDislike: state.isDislike ? false : true,
+        isDislike: !state.isDislike,
         dislikeCount: state.isDislike ? state.dislikeCount - 1 : state.dislikeCount + 1,
         likesCount: state.isLike ? state.likesCount - 1 : state.likesCount,
     }),
